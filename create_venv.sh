4b32677be38b2202139f36809107786be3d38fdc
5pirit5eal <rubeneschulze@googlemail.com>
2024-03-08 10:14:27 +0000

    added venv creation

--- create_venv.sh
+++ create_venv.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+
+# Check if .venv folder exists
+if [ -d ".venv" ]; then
+    # Prompt user for confirmation
+    read -p "The .venv folder already exists. Do you want to delete it? (y/n): " confirm
+
+    # Check user's response
+    if [ "$confirm" = "y" ]; then
+        # Delete .venv folder
+        rm -rf .venv
+        echo "Deleted .venv folder."
+    else
+        echo "Skipping deletion of .venv folder."
+        return
+    fi
+fi
+
 python -m venv .venv --prompt coverletter
 source .venv/bin/activate
 pip install -r requirements.txt
